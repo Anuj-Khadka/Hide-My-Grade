cc4f39524818d222c8b8797a830820347b1f1238
Anuj-Khadka <anujkhadka008@gmail.com>
2024-01-17 00:03:19 -0600

    Initiate ManagMate

---
+++ frontend/ManagMate/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../assets/css/Navbar.css'
+
+function Navbar() {
+    return (
+        <>
+            <h1 id='brand-name'>
+                ManagMate
+            </h1>
+        </>
+    )
+}
+
+export default Navbar
