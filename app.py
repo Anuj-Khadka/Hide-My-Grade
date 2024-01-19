f9513050317afbf6d6d1cc3d7af9a9f2849360b1
Anuj-Khadka <anujkhadka008@gmail.com>
2024-01-18 23:49:34 -0600

    Initiate Backend

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    # return "<p>Hello, World!</p>"
+
+    # return render_template('./frontent/ManagMate/src')
+
+    return {
+        "name": "noname",
+        "age": 12
+    }
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
